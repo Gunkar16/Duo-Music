34c14ccc18e770a19a5b877aeca98df6abe52a35
Gunkar16 <76874019+Gunkar16@users.noreply.github.com>
2021-06-29 18:18:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,75 @@
+song1 = "";
+song2 = "";
+
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+scoreLeftWrist = 0;
+StatusSong = "";
+
+scoreRightWrist = 0;
+StatusSong2 = "";
+
+
+function preload(){
+    song1 = loadSound("Sorry_By_Alan_Walker.mp3");
+    song2= loadSound("Tones-and-I-Dance-Monkey.mp3");
+}
+
+function setup(){
+    Canvas = createCanvas(350,250);
+    Canvas.position(580,250);
+    Video = createCapture(VIDEO);
+    Video.hide();
+
+    poseNet = ml5.poseNet(Video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Model Loaded")
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
+}
+
+function draw(){
+    image(Video,0,0,350,250);
+    fill('orange');
+    stroke('orange');
+    SongStatus = song1.isPlaying()
+    if(scoreLeftWrist > 0.2){
+        console.log("Left")
+        circle(leftWristX,leftWristY,20);
+        song2.stop();
+
+        if(SongStatus = "false"){
+            song1.play();
+        }
+    }
+
+    if(scoreRightWrist > 0.2){
+        console.log("Right")
+        circle(rightWristX,rightWristY,20);
+        song1.stop();
+        if(SongStatus2 = "false"){
+            song2.play();
+        }
+    }
+
+}
+
